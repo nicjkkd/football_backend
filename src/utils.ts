c7545f047e57b2a8212c5e6847b50871ae0f2898
nicjkkd <kolya.lukianov.2018@gmail.com>
2025-01-28 16:22:27 +0200

    feat/added test worker to learn how to use it

--- src/utils.ts
+++ src/utils.ts
@@ -46,3 +46,8 @@ export const sendResponseWithBroadcast = ({
   broadcast(JSON.stringify(broadcastData));
   response.json(responseData);
 };
+
+export const fibonacci = (n: number): number => {
+  if (n <= 1) return n;
+  return fibonacci(n - 1) + fibonacci(n - 2);
+};
